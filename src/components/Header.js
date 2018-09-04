9b933def320ce57d091c341de85245a30430ad7c
Christopher Arsenault <christopher.arsenault82@gmail.com>
2018-09-03 21:48:33 -0400

    finished responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <header className="header">
-            <h1>Nourish Our Souls</h1>
-            <nav className="topNav">
-				<ul className="topNavLinks">
-                    <Link to="/" className="navLink"><li>Home</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to="/admin" className="navLink"><li>Add Post</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to="/aboutus" className="navLink"><li>About Us</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to="/contactus" className="navLink"><li>Contact Us</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to={`/blog-list/recipes`} className="navLink" ><li key="recipes">Recipes</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to={`/blog-list/restaurants`} className="navLink" ><li key="restaurants">Restaurants</li></Link>
-                    <li className="navSeperator"> | </li>
-                    <Link to={`/blog-list/foodadventures`} className="navLink" ><li key="foodadventures">Food Adventures</li></Link>
-				</ul>
-			</nav>
-        </header>
-    );
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isActive: false
+        }
+    }
+    toggleNavMenu = (e) => {
+        this.setState({
+            isActive: !this.state.isActive
+        })
+    }
+    render() {
+        return (
+            <header className="header">
+                <h1>Nourish Our Souls</h1>
+                <nav className="topNav">
+                    <p onClick={this.toggleNavMenu} className="topNavHamburgerIcon"><i className="fas fa-bars"></i></p>
+                    <div className={this.state.isActive ? 'topNavHamburgerMenu show' : 'topNavHamburgerMenu'}>
+                        <ul className="topNavLinks navColumns">
+                            <li onClick={this.toggleNavMenu} className="exitResponsive"><i className="fas fa-times"></i></li>
+                            <Link to="/" onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden"><li>Home</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to="/admin" onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden"><li>Add Post</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to="/aboutus" onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden"><li>About Us</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to="/contactus" onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden"><li>Contact Us</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to={`/blog-list/recipes`} onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden" ><li key="recipes">Recipes</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to={`/blog-list/restaurants`} onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden" ><li key="restaurants">Restaurants</li></Link>
+                            <li className="navSeperator"> | </li>
+                            <Link to={`/blog-list/foodadventures`} onClick={this.toggleNavMenu} className="navLink topNavItemHidden topNavLinkHidden" ><li key="foodadventures">Food Adventures</li></Link>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
 };
 
 export default Header;
